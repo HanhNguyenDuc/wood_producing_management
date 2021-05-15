199d0933c2c1a99d8cc4f8bf8e2f12dbb13bf16a
unknown <ducminhvu304@gmail.com>
2021-05-15 15:06:18 +0700

    chuc nang add new material

--- wood_producing/urls.py
+++ wood_producing/urls.py
@@ -1,5 +1,5 @@
 from wood_producing.views.api import delete_task
-from wood_producing.views.storage_manager import ImportMaterialFromProvider, MaterialManagement, ProductManagement
+from wood_producing.views.storage_manager import EditMaterial, ImportMaterialFromProvider, AddMaterial, MaterialBase, ProductManagement
 from wood_producing.views.statistic import MaterialStatistic, ProductionStatistic
 from wood_producing.views.seller import CreateOrder, PublishOrder
 from django.urls import path
@@ -19,8 +19,10 @@ urlpatterns = [
     path('statistic/production', views.ProductionStatistic.as_view(), name='production_statistic'),
     path('statistic/profit', views.ProfitStatistic.as_view(), name='profit_statistic'),
     path('statistic/material', views.MaterialStatistic.as_view(), name='material_statistic'),
+    path('storage_manager/material_base',views.MaterialBase.as_view(), name="material_base"),
     path('storage_manager/import_material', views.ImportMaterialFromProvider.as_view(), name='import_material'),
-    path('storage_manager/material_manage', views.MaterialManagement.as_view(), name='material_manage'),
+    path('storage_manager/add_material', views.AddMaterial.as_view(), name='add_material'),
+    path('storage_manager/edit_material', views.EditMaterial.as_view(), name='edit_material'),
     path('storage_manager/product_manage', views.ProductManagement.as_view(), name='product_manage'),
     path('api/task/delete_task', views.delete_task, name='delete_task'),
     path('', views.Index.as_view(), name='index')
--- wood_producing/views/storage_manager.py
+++ wood_producing/views/storage_manager.py
@@ -7,24 +7,71 @@ class ImportMaterialFromProvider(RoleRequiredView):
     def update_post_context(self, request, *args, **kwargs):
         return super().update_post_context(request, *args, **kwargs)
 
+class MaterialBaseForm(forms.Form):
+    search_keyword = forms.CharField()
 
 
-class MaterialManagement(RoleRequiredView):
+class MaterialBase(RoleRequiredView):
+    user_role = 2 
+    form = MaterialBaseForm
+    template_path = "wood_producing/producing_manager/material_base.html"
+    direct_url={}
+    
+    def update_get_context(self, request, *args, **kwargs):
+
+        
+        return None
+
+    def update_post_context(self, request, *args, **kwargs):
+        return super().update_post_context(request, *args, **kwargs)
+
+class AddMaterialForm(forms.Form):
+    material_name = forms.CharField()
+    type = forms.CharField()
+
+class AddMaterial(RoleRequiredView):
+    user_role = 2
+    form = AddMaterialForm
+    template_path = "wood_producing/producing_manager/material_add.html"
+    direct_url = {
+    }
+    
     """ Get Management view
         Delete, Add, Update be implemented in api.py
     """
     def update_get_context(self, request, *args, **kwargs):
-        return super().update_get_context(request, *args, **kwargs)
+
+        return None
     
     def update_post_context(self, request, *args, **kwargs):
-        return super().update_post_context(request, *args, **kwargs)
+        material = Material.objects.create(
+            name = self.cleaned_data.get('material_name'),
+            type = self.cleaned_data.get('type'),
+            desc = "test",
+            discriminator = 1
+        )
+        material.save()
+        return None
+
+class EditMaterial(RoleRequiredView):
+    user_role = 2
+    form = None
+    template_path = "wood_producing/producing_manager/material_edit.html"
+    direct_url={}
+    
+    def update_get_context(self, request, *args, **kwargs):
+        return None
+
+    def update_post_context(self, request, *args, **kwargs):
+        return None
+
 
 class ProductManagement(RoleRequiredView):
     """ Get Management view
         Delete, Add, Update be implemented in api.py
     """
     def update_get_context(self, request, *args, **kwargs):
-        return super().update_get_context(request, *args, **kwargs)
+        return None
     
     def update_post_context(self, request, *args, **kwargs):
-        return super().update_post_context(request, *args, **kwargs)
+        return None
