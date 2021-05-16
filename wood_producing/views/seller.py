337393f535f1a434be98499808c0a4585670de5e
Nguyễn Đức Hưng <hungnguyenw420@gmail.com>
2021-05-16 12:54:11 +0700

    Init

--- wood_producing/views/seller.py
+++ wood_producing/views/seller.py
@@ -1,4 +1,45 @@
 from .views import *
+import logging
+
+class ListOrder(RoleRequiredView):
+    user_role = 5
+    form = None
+    template_path = "wood_producing/seller/list_order.html"
+    direct_url = {
+        "order_detail": "seller/order_detail",
+        "change_password": "user/change_password",
+        "logout": "user/logout",
+    }
+    def update_get_context(self, request, *args, **kwargs):
+        search_query = request.GET.get("order_name")
+        customer_id = request.GET.get("customer_id")
+        page_size = settings.PAGE_SIZE
+        page_num = request.GET.get("page_num")
+        user_id = request.user.id
+        orders = Order.objects.all().filter(userid=user_id)
+        for x in orders:
+            print(x)
+        p = Paginator(orders, page_size)
+        cur_page = p.page(1)
+        if page_num is not None:
+            cur_page = p.page(page_num)
+        self.context["num_pages"] = p.num_pages
+        self.context["pages"] = range(1, p.num_pages+1)
+        self.context["orders"] = cur_page.object_list
+        return None
+
+    def update_post_context(self, request, *args, **kwargs):
+        return super().update_post_context(request, *args, **kwargs)
+
+class OrderDetail(RoleRequiredView):
+    user_role = 5
+    form = None
+    template_path = "wood_producing/seller/order_detail.html"
+    def update_get_context(self, request, *args, **kwargs):
+        return super().update_post_context(request, *args, **kwargs)
+
+    def update_post_context(self, request, *args, **kwargs):
+        return super().update_post_context(request, *args, **kwargs)
 
 class ListOrder(RoleRequiredView):
     user_role = 5
